689bdd39fe5a9d1e448a23f7a77e0d801c7ab06f
estefanymoreira <estefanymoreira06@gmail.com>
2025-09-30 21:02:43 -0300

    modificacion de clase carrera

--- src/CasoEstudioUTEC/Carrera.java
+++ src/CasoEstudioUTEC/Carrera.java
@@ -1,43 +1,25 @@
 package CasoEstudioUTEC;
 
 public class Carrera {
-
-    private String id;
+    private int id;
+    private String codigo;
     private String nombre;
-    private int codigo;
     private String plan;
 
-    public Carrera(String id, String nombre, int codigo, String plan) {
+    public Carrera(int id, String codigo, String nombre, String plan) {
         this.id = id;
-        this.nombre = nombre;
         this.codigo = codigo;
+        this.nombre = nombre;
         this.plan = plan;
     }
 
-    public String getId() {
-        return id;
-    }
+    public int getId() { return id; }
+    public String getCodigo() { return codigo; }
+    public String getNombre() { return nombre; }
+    public String getPlan() { return plan; }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getCodigo() {
-        return codigo;
-    }
-
-    public String getPlan() {
-        return plan;
-    }
-
-
-    @java.lang.Override
-    public java.lang.String toString() {
-        return "Carrera{" +
-                "id=" + id +
-                ", nombre='" + nombre + '\'' +
-                ", codigo=" + codigo +
-                ", plan='" + plan + '\'' +
-                '}';
+    @Override
+    public String toString() {
+        return "Carrera{" + "id=" + id + ", codigo='" + codigo + '\'' + ", nombre='" + nombre + '\'' + ", plan='" + plan + '\'' + '}';
     }
 }
