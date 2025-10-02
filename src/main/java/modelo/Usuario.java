4dcbe00ebe6bf2dfdb76f6910220343f4281e44a
IgnacioLaraDev <ignacio.arevalo5657@gmail.com>
2025-10-01 22:49:06 -0300

    "Actualizacion de la clase Usuario y Funcionario

--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -1,24 +1,27 @@
 package main.java.modelo;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.ArrayList;
 
-public class Usuario {
+/**
+ * Clase abstracta Usuario.
+ * Nunca se instancia directamente; siempre es Estudiante o Funcionario.
+ */
+public abstract class Usuario {
 
-    private String id;
+    private int id;
     private String ci;
     private String username;
     private String password;
     private String nombre;
     private String apellido;
     private String correo;
-    private List<String> telefonos = new ArrayList<String>();
-    private boolean estadoActivo = true;
-    private LinkedList<Notificacion> listaNotificaciones = new LinkedList<Notificacion>();
-    private List<Direccion> direcciones = new ArrayList<Direccion>();
+    private List<String> telefonos = new ArrayList<>();
+    private List<Direccion> direcciones = new ArrayList<>();
+    private LinkedList<Notificacion> listaNotificaciones = new LinkedList<>();
 
-    public Usuario(String id, String ci, String username, String password, String nombre, String apellido, String correo, String telefono, Direccion direccion) {
+    public Usuario(int id, String ci, String username, String password,String nombre, String apellido, String correo,String telefono, Direccion direccion) {
         this.id = id;
         this.ci = ci;
         this.username = username;
@@ -26,104 +29,36 @@ public class Usuario {
         this.nombre = nombre;
         this.apellido = apellido;
         this.correo = correo;
-        telefonos.add(telefono);
-        direcciones.add(direccion);
+        if(telefono!=null){
+            telefonos.add(telefono);
+        }
+        if(direccion!=null){
+            direcciones.add(direccion);
+        }
     }
 
-    //Constructor para llamarlo en la clase Notificacion
-    //Se va a usar para el constructor de Notificaciones, super()
-    //Para usarlo en el futuro
-//    public Usuario(String id,
-//                   String nombre,
-//                   String apellido,
-//                   String correo) {
-//        this.id = id;
-//        this.nombre = nombre;
-//        this.apellido = apellido;
-//        this.correo = correo;
-//    }
+    // Getters / setters principales
+    public void setId(int id) { this.id = id; }
 
-    public String getId() {
-        return id;
-    } // no se realiza setter porque el id no puede cambiarse una vez creado
 
-    public String getUsername() {
-        return username;
-    } // no se realiza setter porque el username no puede cambiarse una vez creado
+    public int getId() { return id; }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getApellido() { return apellido; }
+    public void setApellido(String apellido) { this.apellido = apellido; }
+    public String getCorreo() { return correo; }
+    public void setCorreo(String correo) { this.correo = correo; }
+    public void addTelefono(String telefono) { telefonos.add(telefono); }
+    public List<Direccion> getDirecciones() { return direcciones; }
+    public void addDireccion(Direccion direccion) { direcciones.add(direccion); }
+    public void addNotificacion(Notificacion n) { listaNotificaciones.add(n); }
+    public LinkedList<Notificacion> getListaNotificaciones() { return listaNotificaciones; }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password= password;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String getTelefonos() {
-        return telefonos.toString();
-    }
-
-    public void addTelefono(String telefono) {
-        telefonos.add(telefono);
-    }
-
-    public boolean isEstadoActivo() {
-        return estadoActivo;
-    }
-
-    public void setEstadoActivo(boolean estadoActivo) {
-        this.estadoActivo = estadoActivo;
-    }
-
-    public void addNotificacion(Notificacion notificacion) {
-        listaNotificaciones.add(notificacion);
-    }
-
-    public LinkedList<Notificacion> getListaNotificaciones() {
-        return listaNotificaciones;
-    }
-
-    public List<Direccion> getDirecciones() {
-        return direcciones;
-    }
-
-    public String getCi() {
-        return ci;
-    }
-
-    public void addDireccion(Direccion direccion) {
-        direcciones.add(direccion);
-    }
-
-    // metodo toString del segundo constructor
     @Override
     public String toString() {
-        return "Usuario: " + "id= '" + id + '\'' + "CI= '"+ci+"' , nombre= '" + nombre + '\'' + ", apellidos= '" + apellido + '\'' + ", correo= '" + correo + '\''+", Direcciones: '" + direcciones + '\'';
+        return "Usuario{id='" + id + "', ci='" + ci + "', username='" + username + "', nombre='" + nombre + "', apellido='" + apellido + "', correo='" + correo + "', telefonos=" + telefonos + "}";
     }
-
-
 }
