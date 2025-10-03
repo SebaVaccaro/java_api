b0b4afdfe906b520a4fb13835eff244593ec91c6
Cleris <cleris.mill@gmail.com>
2025-10-02 21:28:22 -0300

    modificación Seguimiento

--- src/main/java/modelo/Seguimiento.java
+++ src/main/java/modelo/Seguimiento.java
@@ -5,13 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Seguimiento {
-    private Integer id; // id de tabla seguimientos
+    private int id; // id de tabla seguimientos
     private Estudiante estudiante;
     private LocalDate fechaInicio;
     private LocalDate fechaCierre;
     private List<Funcionario> participantes = new ArrayList<>();
     private List<InstanciaComun> instancias = new ArrayList<>();
-    private boolean estadoActivo = true;
+    //  private boolean estadoActivo = true; //
+    private boolean activo = true;
+
 
     public Seguimiento(Integer id, Estudiante estudiante, LocalDate fechaInicio, List<Funcionario> participantes) {
         this.id = id;
@@ -20,20 +22,67 @@ public class Seguimiento {
         this.participantes = participantes;
     }
 
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
-    public Estudiante getEstudiante() { return estudiante; }
-    public void setEstudiante(Estudiante estudiante) { this.estudiante = estudiante; }
-    public LocalDate getFechaInicio() { return fechaInicio; }
-    public void setFechaInicio(LocalDate fechaInicio) { this.fechaInicio = fechaInicio; }
-    public LocalDate getFechaCierre() { return fechaCierre; }
-    public void setFechaCierre(LocalDate fechaCierre) { this.fechaCierre = fechaCierre; }
-    public List<Funcionario> getParticipantes() { return participantes; }
-    public void addParticipante(Funcionario funcionario) { participantes.add(funcionario); }
-    public List<InstanciaComun> getInstancias() { return instancias; }
-    public void addInstancia(InstanciaComun instancia) { instancias.add(instancia); }
-    public boolean isEstadoActivo() { return estadoActivo; }
-    public void setEstadoActivo(boolean estadoActivo) { this.estadoActivo = estadoActivo; }
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaCierre() {
+        return fechaCierre;
+    }
+
+    public void setFechaCierre(LocalDate fechaCierre) {
+        this.fechaCierre = fechaCierre;
+    }
+
+    public List<Funcionario> getParticipantes() {
+        return participantes;
+    }
+
+    public void addParticipante(Funcionario funcionario) {
+        participantes.add(funcionario);
+    }
+
+    public List<InstanciaComun> getInstancias() {
+        return instancias;
+    }
+
+    public void addInstancia(InstanciaComun instancia) {
+        instancias.add(instancia);
+    }
+
+    // public boolean isEstadoActivo() { return estadoActivo; }
+//
+    public boolean isActivo() {
+        return activo;
+    }
+
+
+    // public void setEstadoActivo(boolean estadoActivo) { this.estadoActivo = estadoActivo; }
+//
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
 
     @Override
     public String toString() {
@@ -44,7 +93,7 @@ public class Seguimiento {
                 ", fechaCierre=" + fechaCierre +
                 ", participantes=" + participantes +
                 ", instancias=" + instancias +
-                ", estadoActivo=" + estadoActivo +
+                ", activo=" + activo +
                 '}';
     }
 }
