45190242fcd717a3a145f2096ce917b516539767
wilmar <wilmar.hellbusch@estudiantes.utec.edu.uy>
2025-11-13 13:23:39 -0300

    Elimino las llamadas encadenadas a eliminarFisico de ArchivoAdjuntoDAO (Servicio, Proxy)

--- src/main/java/PROXY/ArchivoAdjuntoProxy.java
+++ src/main/java/PROXY/ArchivoAdjuntoProxy.java
@@ -67,11 +67,5 @@ public class ArchivoAdjuntoProxy {
         }
         return service.eliminarArchivo(idArchivo);
     }
-    // Eliminar archivo físicamente (solo admin)
-    public boolean eliminarFisico(int idArchivo) throws Exception {
-        if (!validarUsuario.esAdministrador()) {
-            throw new SecurityException("Solo un administrador puede eliminar físicamente este archivo.");
-        }
-        return service.eliminarFisico(idArchivo);
-    }
+
 }
